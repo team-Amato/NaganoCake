4568859c99451821ec599b785251baaa44188fdd
EC2 Default User <ec2-user@ip-172-31-31-120.ap-northeast-1.compute.internal>
2023-09-23 11:49:29 +0000

    商品追加、一覧、編集画面コピー完了

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,48 @@
 class Admin::ItemsController < ApplicationController
+   # before_action :authenticate_admin!
+  before_action :set_product, only: %i[show edit update]
+
+  def index
+    @items = Item.page(params[:page])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :explanation, :genre, :is_active, :item_image)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :explanation, presence: true
+  validates :is_active, presence: true
+  validates :item_image, presence: true
+
+  has_one_attached :item_image
+  belongs_to :genre
 end
