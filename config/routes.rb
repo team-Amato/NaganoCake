e6b26a68cb552f4e5f237b0bfca291712912a729
yuka00000 <yukayamada0914@gmail.com>
2023-09-23 12:14:20 +0000

    [Add]新規登録画面（customer）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # 顧客用
   # URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {
+  devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
@@ -18,40 +18,26 @@ Rails.application.routes.draw do
     get '/customers/mypage' => 'customers#show'
     get 'customers/check'
     patch 'customers/withdraw'
-  end
+    
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete 'cart_items/destroy_all'
   
-
-  resources :cart_items, only: [:index, :update, :destroy, :create]
-  delete 'cart_items/destroy_all'
-
-  resources :orders, only: [:new, :create, :index, :show]
-  post 'orders/confirm'
-  get 'orders/thanks'
-
-  resources :items, only: [:index, :show]
-
-  resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :orders, only: [:new, :create, :index, :show]
+    post 'orders/confirm'
+    get 'orders/thanks'
+  
+    resources :items, only: [:index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
 
   #admin
   namespace :admin do
-    get '/admin/sign_in' => 'sessions#new'
-    post '/admin/sign_in' => 'sessions#create'
-    delete '/admin/sign_out' => 'sessions#destroy'
-  end
-
-  namespace :admin do
-    get '/admin' => 'homes#top'
-  end
-
-  namespace :admin do
+    get '/' => 'homes#top'
     resources :order_details, only: [:update]
     resources :items, only: [:edit, :update, :new, :create, :index, :show]
     resources :genres, only: [:edit, :update, :index, :create]
     resources :customers, only: [:edit, :update, :index, :show]
-     resources :orders, only: [:show, :update]
+    resources :orders, only: [:show, :update]
   end
-
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
