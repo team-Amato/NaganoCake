eab72e2dc8e8943fa44957430e0dab7882219f91
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-24 02:20:15 +0000

    [Update]管理者ログインtopページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,4 +1,2 @@
 class Admin::CustomersController < ApplicationController
-    
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,24 +12,40 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   
-  scope module: :public do
+  #scope module: :public do
+   # root to: "homes#top"
+   # get '/about' => "homes#about"
+   # get '/customers/mypage' => 'customers#show'
+  #  get 'customers/check'
+    #patch 'customers/withdraw'
+  #end
+  
+  
+    scope module: :public do
     root to: "homes#top"
     get '/about' => "homes#about"
     get '/customers/mypage' => 'customers#show'
     get 'customers/check'
     patch 'customers/withdraw'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete 'cart_items/destroy_all'
+    resources :orders, only: [:new, :create, :index, :show]
+    post 'orders/confirm'
+    get 'orders/thanks'
+    resources :items, only: [:index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
   
   namespace :public do
   resources :customers, only: [:show, :edit, :update]
 end
 
-  resources :cart_items, only: [:index, :update, :destroy, :create]
-  delete 'cart_items/destroy_all'
+ # resources :cart_items, only: [:index, :update, :destroy, :create]
+ # delete 'cart_items/destroy_all'
 
-  resources :orders, only: [:new, :create, :index, :show]
-  post 'orders/confirm'
-  get 'orders/thanks'
+ # resources :orders, only: [:new, :create, :index, :show]
+ # post 'orders/confirm'
+  #get 'orders/thanks'
 
   resources :items, only: [:index, :show]
 
@@ -42,19 +58,26 @@ end
     delete '/admin/sign_out' => 'sessions#destroy'
   end
 
-  namespace :admin do
-    get '/admin' => 'homes#top'
-  end
+ # namespace :admin do
+   # get '/admin' => 'homes#top'
+  #end
 
-  namespace :admin do
+  #namespace :admin do
+    #resources :order_details, only: [:update]
+    #resources :items, only: [:edit, :update, :new, :create, :index, :show]
+    #resources :genres, only: [:edit, :update, :index, :create]
+    #resources :customers, only: [:edit, :update, :index, :show]
+    #resources :orders, only: [:show, :update]
+  #end
+
+namespace :admin do
+    get '/' => 'homes#top'
     resources :order_details, only: [:update]
     resources :items, only: [:edit, :update, :new, :create, :index, :show]
     resources :genres, only: [:edit, :update, :index, :create]
     resources :customers, only: [:edit, :update, :index, :show]
-     resources :orders, only: [:show, :update]
+    resources :orders, only: [:show, :update]
   end
 
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
