d58288f7c4e5bb08855f87f3a0d946a13d8e2986
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-24 07:08:57 +0000

    [Add]マイページ編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -12,14 +12,14 @@ class Public::CustomersController < ApplicationController
     def update
         @customer = current_customer
         if @customer.update(customer_params)
-         redirect_to public_customer_path(@customer), notice: "登録情報が更新されました。"
+         redirect_to customers_mypage_path(@customer), notice: "登録情報が更新されました。"
         else
         render :edit
         end
     end
     
     def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :email)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :email,:phone_number)
     end
     
 end
