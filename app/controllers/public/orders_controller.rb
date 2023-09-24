64a8db6164fb1eb4e7509764ea6c99612a66e8cb
EC2 Default User <ec2-user@ip-172-31-31-120.ap-northeast-1.compute.internal>
2023-09-24 08:38:36 +0000

    注文途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,104 +1,105 @@
 class Public::OrdersController < ApplicationController
   def new
+    @cart_items = current_customer.cart_items
   end
-  
+
   def create
-    @order = Order.new
-    @order.member_id = current_member.id
-    @order.postage = 800
-    @cart_items = CartItem.where(member_id: current_member.id)
-    ary = []
-    @cart_items.each do |cart_item|
-      ary << cart_item.item.price*cart_item.quantity
-    end
-    @cart_items_price = ary.sum
-    @order.total_price = @order.postage + @cart_items_price
-    @order.pay_method = params[:order][:pay_method]
-    if @order.pay_method == "credit_card"
-      @order.status = 1
-    else
-      @order.status = 0
-    end
-    # 配送先の選択
-    address_type = params[:order][:address_type]
-      case address_type
-    when "member_address"
-      @order.post_code = current_member.post_code
-      @order.address = current_member.address
-      @order.name = current_member.last_name + current_member.first_name
-    when "registered_address"
-      Addresse.find(params[:order][:registered_address_id])
-      selected = Addresse.find(params[:order][:registered_address_id])
-      @order.post_code = selected.post_code
-      @order.address = selected.address
-      @order.name = selected.name
-    when "new_address"
-      @order.post_code = params[:order][:new_post_code]
-      @order.address = params[:order][:new_address]
-      @order.name = params[:order][:new_name]
-    end
-    
-    if @order.save
-      if @order.status == 0
-        @cart_items.each do |cart_item|
-          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.price, quantity: cart_item.quantity, making_status: 0)
-        end
-      else
-        @cart_items.each do |cart_item|
-          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.price, quantity: cart_item.quantity, making_status: 1)
-        end
-      end
-      @cart_items.destroy_all
-      redirect_to orders_thanks_path
-    else
-      render :items
-    end
-  end    
+    customer = current_customer
+
+		# sessionを使ってデータを一時保存
+		session[:order] = Order.new
+
+		cart_items = current_customer.cart_items
+
+		# total_paymentのための計算
+		sum = 0
+		cart_items.each do |cart_item|
+			sum += (cart_item.item.price_without_tax * 1.1).floor * cart_item.quantity
+		end
+
+		session[:order][:postage] = 800
+		session[:order][:total_price] = sum + session[:order][:postage]
+		session[:order][:status] = 0
+		session[:order][:customer_id] = current_customer.id
+		# ラジオボタンで選択された支払方法のenum番号を渡している
+		session[:order][:payment_method] = params[:method].to_i
+
+		# ラジオボタンで選択されたお届け先によって条件分岐
+		destination = params[:a_method].to_i
+
+		# ご自身の住所が選択された時
+		if destination == 0
+
+			session[:order][:post_code] = customer.post_code
+			session[:order][:address] = customer.address
+			session[:order][:name] = customer.last_name + customer.first_name
+
+		# 登録済住所が選択された時
+		elsif destination == 1
+
+			address = ShippingAddress.find(params[:shipping_address_for_order])
+			session[:order][:post_code] = address.postal_code
+			session[:order][:address] = address.address
+			session[:order][:name] = address.name
+
+		# 新しいお届け先が選択された時
+		elsif destination == 2
+
+			session[:new_address] = 2
+			session[:order][:post_code] = params[:post_code]
+			session[:order][:address] = params[:address]
+			session[:order][:name] = params[:name]
+
+		end
+
+		# お届け先情報に漏れがあればリダイレクト
+		if session[:order][:post_code].presence && session[:order][:address].presence && session[:order][:name].presence
+			redirect_to new_order_path
+		else
+			redirect_to new_order_path
+		end
+
   end
-    
+
   def index
      @orders = Order.where(member_id: current_member.id).order(created_at: :desc).#注文履歴を降順で並べる
   end
-  
+
   def show
     @order = Order.find(params[:id])
     @order_details= OrderDetail.where(order_id: @order.id)
   end
-  
+
   def confirm
-    @cart_items = CartItem.where(member_id: current_member.id)
-    @postage = 800   #送料を800円で固定する
-    @selected_pay_method = params[:order][:pey_method]
-      
-    # 商品合計額の計算
-    ary = []
-    @cart_items.each do |cart_item|
-      ary << cart_item.item.price*cart_item.quantity
-    end
-    @cart_items_price = ary.sum
-    @total_price = @postage + @cart_items_price
-    
-    # 配送先の確認
-    @address_type = params[:order][:address_type]
-    case @address_type
-    when "member_address"
-      @selected_address = current_member.post_code + " " + current_member.address + " " + current_member.last_name + current_member.first_name then
-    when "registered_address"
-    unless params[:order][:registered_address_id] == ""
-      selected = Address.find(params[:order][:registered_address_id]
-      @selected_address = selected.post_code + "" + selected.address + "" + selected.name then
-	  else	 
-	   render :new
-	  end
-    when "new_address"
-    unless params[:order][:new_post_code] == "" && params[:order][:new_address] == "" && params[:order][:new_name] == ""
-	    @selected_address = params[:order][:new_post_code] + " " + params[:order][:new_address] + " " + params[:order][:new_name]
-	  else
-	   render :new
-	  end
-  end     
+    order = Order.new(session[:order])
+		order.save
+
+		if session[:new_address]
+			shipping_address = current_customer.shipping_addresses.new
+			shipping_address.post_code = order.post_code
+			shipping_address.address = order.address
+			shipping_address.name = order.name
+			shipping_address.save
+			session[:new_address] = nil
+		end
+
+		# 以下、order_detail作成
+		cart_items = current_customer.cart_items
+		cart_items.each do |cart_item|
+			order_detail = OrderDetail.new
+			order_detail.order_id = order.id
+			order_detail.item_id = cart_item.item.id
+			order_detail.quantity = cart_item.quantity
+			order_detail.making_status = 0
+			order_detail.price = (cart_item.item.price_without_tax * 1.1).floor
+			order_detail.save
+		end
+
+		# 購入後はカート内商品削除
+		cart_items.destroy_all
   end
-  
+  end
+
   def thanks
   end
 
