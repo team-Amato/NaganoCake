1a991de09dd09d08e7144570ab5ab7cb8460ba16
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-09-24 10:26:36 +0000

    マージ用

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,13 @@
 class Public::CartItemsController < ApplicationController
 
   def create
+    @cart_item = Cart_item.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
   end
 
+
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
+  end
 end
