eb7f1d9d9483cf53daaa3559f8cd04ee148e8d97
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-24 04:06:17 +0000

    [Update]コンフリクト解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,13 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-    
-before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-    root_path
-=======
   before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
     items_path
->>>>>>> origin/develop
   end
 
   protected
 
   def configure_permitted_parameters
-<<<<<<< HEAD
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
-=======
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :address, :phone_number, :post_code])
-  end
->>>>>>> origin/develop
-end
+end  
