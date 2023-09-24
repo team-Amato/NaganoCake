3d5cdb03098e63812f87ec262232df3651bf4a89
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-09-24 12:24:15 +0000

    カート一覧表示

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,15 @@
 class Public::CartItemsController < ApplicationController
 
+  def index
+    @cart_items = current_customer.cart_items
+  end
+
   def create
-    @cart_item = Cart_item.new(cart_item_params)
+    @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
+    if @cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
 
