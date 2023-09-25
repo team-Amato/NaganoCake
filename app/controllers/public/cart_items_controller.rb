7c59add334a602b1fc04da160c274cc98ce567d9
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-09-25 07:53:58 +0000

    カート機能:同一商品の場合数量のみ増やす機能 実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,23 @@
 class Public::CartItemsController < ApplicationController
 
-  before_action :setup_item!, only: [:create]
 
   def index
     @cart_items = current_customer.cart_items
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    if @cart_item.save
-      redirect_to cart_items_path
+    # 同じ商品が既にある場合の処理
+    # 同一商品のレコードをcart_itemからfind_byで検索
+    @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]) 
+    if @cart_item
+      @amount = @cart_item.amount.to_i + params[:cart_item][:amount].to_i
+      @cart_item.update(amount: @amount)
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
     end
+    redirect_to cart_items_path
   end
 
   def destroy
@@ -26,15 +32,10 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path
   end
 
-
   private
 
   def cart_item_params
       params.require(:cart_item).permit(:item_id, :amount)
   end
 
-  def setup_item
-    byebug
-    @cart_item = CartItem.find_by(item_id: params[:item_id])
-  end
 end
