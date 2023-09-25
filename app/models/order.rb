de73df141a5c775141715a2a05c400bc73580e37
yuka00000 <yukayamada0914@gmail.com>
2023-09-25 05:23:22 +0000

    [Add]注文情報詳細

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  has_many :order_details, dependent: :destroy
+enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
 end
