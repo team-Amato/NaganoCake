ee88385e10b1761a28c83d1fb86bf39e79b34690
EC2 Default User <ec2-user@ip-172-31-31-120.ap-northeast-1.compute.internal>
2023-09-26 09:05:14 +0000

    注文途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,63 +5,28 @@ class Public::OrdersController < ApplicationController
     @addresses = Address.where(customer_id: current_customer.id)
   end
 
+# 注文の確定をするアクション
   def create
     @customer = current_customer
+		@cart_items = current_customer.cart_items.all
 
-		# sessionを使ってデータを一時保存
-		session[:order] = Order.new
+		@order = current_customer.orders.new(order_params)
+    if @order.save
+			cart_items.each do |cart|
+			order_item = OrderItem.new
+      order_item.item_id = cart.item_id
+      order_item.order_id = @order.id
+      order_item.order_amount = cart.amount
 
-		@cart_items = current_customer.cart_items
-
-		# total_paymentのための計算
-		sum = 0
-		@cart_items.each do |cart_item|
-			sum += cart_item.item.price*cart_item.amount
-		end
-
-		session[:order][:postage] = 800
-		session[:order][:total_price] = sum + session[:order][:postage]
-		session[:order][:status] = 0
-		session[:order][:customer_id] = current_customer.id
-		# ラジオボタンで選択された支払方法のenum番号を渡している
-		session[:order][:payment_method] = params[:method].to_i
-
-		# ラジオボタンで選択されたお届け先によって条件分岐
-		destination = params[:a_method].to_i
-
-		# ご自身の住所が選択された時
-		if destination == 0
-			session[:order][:post_code] = @customer.post_code
-			session[:order][:address] = @customer.address
-			session[:order][:name] = @customer.last_name + @customer.first_name
-		# 登録済住所が選択された時
-		elsif destination == 1
-			address = ShippingAddress.find(params[:shipping_address_for_order])
-			session[:order][:post_code] = address.postal_code
-			session[:order][:address] = address.address
-			session[:order][:name] = address.name
-		# 新しいお届け先が選択された時
-		elsif destination == 2
-			session[:new_address] = 2
-			session[:order][:post_code] = params[:post_code]
-			session[:order][:address] = params[:address]
-			session[:order][:name] = params[:name]
-		end
-
-	  if @order.save
-	      if @order.status == 0
-	        @cart_items.each do |cart_item|
-	          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.price, amount: cart_item.amount, making_status: 0)
-	        end
-	      else
-	        @cart_items.each do |cart_item|
-	          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.price, amount: cart_item.amount, making_status: 1)
-	        end
-	      end
-	      redirect_to orders_confirm_path
-	  else
-	      render new_order_path
-	  end
+      order_item.order_price = cart.item.price
+      order_item.save
+      end
+    redirect_to orders_thanks_path
+    cart_items.destroy_all
+    else
+    @order = Order.new(order_params)
+    render :new
+    end
   end
 
   def index
@@ -69,17 +34,43 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    @order = Order.new(session[:order])
-    @cart_items = CartItem.all
+    @order = Order.new(orders_params)
+    @cart_items = current_customer.cart_items
 
-		if session[:new_address]
-			shipping_address = current_customer.shipping_addresses.new
-			shipping_address.post_code = @order.post_code
-			shipping_address.address = @order.address
-			shipping_address.name = @order.name
-			shipping_address.save
-			session[:new_address] = nil
+    if params[:order][:address_number] == "1"
+    	@order.name = current_customer.first_name + current_customer.last_name
+    	@order.address = current_customer.address
+    	@order.post_code = current_customer.post_code
+
+    elsif params[:order][:address_number] == "2"
+      if Address.exists?(name: params[:order][:registered])
+      	# @address = Address.find(params[:order][:address_id])
+      	@order.name = Address.find(params[:order][:registered]).name
+        @order.address = Address.find(params[:order][:registered]).address
+      else
+      render :new
+      end
+
+    elsif params[:order][:address_number] == "3"
+    	address_new = current_customer.addresses.new(address_params)
+    if address_new.save
+    else
+      render :new
+    end
+    else
+      redirect_to orders_confirm_path
+    end
+      @cart_items = current_customer.cart_items.all
+
+      ary = []
+		  @cart_items.each do |cart_item|
+			 ary << (cart_item.item.with_tax_price).floor * cart_item.amount
 		end
+		  @cart_items_price = ary.sum
+		  @postage = 800
+		  @total_price = @postage + @cart_items_price
+
+      session[:order][:payment_method] = params[:method].to_i
 
 		# 以下、order_detail作成
 		cart_items = current_customer.cart_items
@@ -108,7 +99,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def orders_params
-    params.require(:orders).permit(:customer_id, :post_code, :name, :address, :payment_method, :status, :postage, :total_price)
+    params.require(:order).permit(:customer_id, :post_code, :name, :address, :payment_method, :status, :postage, :total_price)
   end
 
 end
