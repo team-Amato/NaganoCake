0d9bdd34f1839030e02f50aba09a4028fb5b970e
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-09-26 04:28:38 +0000

    [Update]管理者側のヘッダー、編集後アップデート

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -25,6 +25,6 @@ class Admin::CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kan, :first_name_kana, :post_code, :email, :is_deleted, :phone_number)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :email, :is_deleted, :phone_number,:address)
   end
 end
