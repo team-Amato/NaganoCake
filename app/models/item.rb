c3ef8a56e9154da2d8838f38fb61046d4fac3c85
yuka00000 <yukayamada0914@gmail.com>
2023-09-27 09:04:23 +0000

    プルリクし直し

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,6 @@ class Item < ApplicationRecord
 
   has_one_attached :item_image
   belongs_to :genre
-  belongs_to :order
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
 
