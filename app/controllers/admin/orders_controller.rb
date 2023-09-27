fca2e7260c7bb5a06ff7c0a5acb22815b7c14ad3
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-09-27 11:55:26 +0000

    a

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,15 +1,14 @@
 class Admin::OrdersController < ApplicationController
-
   def index
       @orders = Order.all.order(created_at: :desc)
   end
-
   def show
     @order = Order.find(params[:id])
     @orders = Order.all
     @order_items = @order.order_details.all
+    @total_subtotal = 0
+    @postage = 800
   end
-
   def update
     @order = Order.find(params[:id])
     @order_details = OrderDetail.where(order_id: params[:id])
@@ -19,13 +18,7 @@ class Admin::OrdersController < ApplicationController
     end
     redirect_to admin_order_path(@order)
   end
-
-  def subtotal_price
-      price*1.1
-  end
-
   private
-
   def order_params
     params.require(:order).permit(:status)
   end
